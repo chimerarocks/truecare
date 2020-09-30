e3701fb8b4d2c9c1e7b9566659b9c66ddce624a2
João Pedro <joaopedrodslv@gmail.com>
2020-09-29 23:13:47 -0300

    force https on production environment

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use App\Value\TwilioConfiguration;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 use Twilio\Rest\Client;
 
@@ -44,5 +45,9 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
+
+        if (env('APP_ENV') === 'production') {
+            URL::forceScheme('https');
+        }
     }
 }
